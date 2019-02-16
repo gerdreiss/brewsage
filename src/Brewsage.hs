ec51afab6e224fe7607be90e0da1cfb86ad7c5e1
Gerd Reiss <gerdreiss@jscriptive.com>
2019-02-16 21:19:51 +0100

    refactored

--- src/Brewsage.hs
+++ src/Brewsage.hs
@@ -7,14 +7,30 @@ module Brewsage
 import Data.ByteString.Lazy (ByteString)
 import Data.ByteString.Lazy.Char8 (unpack)
 import Data.List.Split
-import System.Exit (ExitCode)
+import System.Exit (ExitCode(ExitSuccess, ExitFailure))
 import System.Process.Typed
 
-
 brewsage :: IO ()
-brewsage = do
-  (exitCode, out, err) <- readProcess "brew list"
-  putStrLn $ "ExitCode = " ++ show exitCode
-  putStrLn $ "Err      = " ++ show err
-  putStrLn "Out      = "
-  mapM_ putStrLn $ splitOn "\n" $ unpack out
+brewsage = brewList >>= mapM_ putStrLn
+
+brewList :: IO [String]
+brewList = readProcess "brew list" >>= processResult
+
+processResult :: (ExitCode, ByteString, ByteString) -> IO [String]
+processResult input =
+  return $
+  case input of
+    (ExitSuccess, out, _) -> formulas out
+    (ExitFailure code, _, err) -> errors code err
+
+formulas :: ByteString -> [String]
+formulas = splitOn "\n" . unpack
+
+errors :: Int -> ByteString -> [String]
+errors code err = ["Error occured:", codeMessage code, errorMessage err]
+
+codeMessage :: Int -> String
+codeMessage code = "Code    -> " ++ show code
+
+errorMessage :: ByteString -> String
+errorMessage err = "Message -> " ++ unpack err
