4e51bd07ddf60eaa2925ef43c1559d74106955c3
Gerd Reiss <gerdreiss@jscriptive.com>
2019-02-16 10:39:09 +0100

    move functionality src/Brewsage.hs

--- src/Brewsage.hs
+++ src/Brewsage.hs
@@ -1,6 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Brewsage
   ( brewsage
   ) where
 
+import Data.ByteString.Lazy (ByteString)
+import Data.ByteString.Lazy.Char8 (unpack)
+import Data.List.Split
+import System.Exit (ExitCode)
+import System.Process.Typed
+
+
 brewsage :: IO ()
-brewsage = putStrLn "brewsage"
+brewsage = do
+  (exitCode, out, err) <- readProcess "brew list"
+  putStrLn $ "ExitCode = " ++ show exitCode
+  putStrLn $ "Err      = " ++ show err
+  putStrLn "Out      = "
+  mapM_ putStrLn $ splitOn "\n" $ unpack out
