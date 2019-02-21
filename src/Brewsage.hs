42306da2e0fbbf3cd136c4277d296a1e719c2bf8
Gerd Reiss <gerdreiss@jscriptive.com>
2019-02-21 20:35:28 +0100

    process formula dependends

--- src/Brewsage.hs
+++ src/Brewsage.hs
@@ -6,31 +6,44 @@ module Brewsage
 
 import Data.ByteString.Lazy (ByteString)
 import Data.ByteString.Lazy.Char8 (unpack)
+import Data.List
 import Data.List.Split
 import System.Exit (ExitCode(ExitSuccess, ExitFailure))
 import System.Process.Typed
 
-
 brewsage :: IO ()
 brewsage = readProcess "brew list" >>= listFormulas >>= processFormulas
-
-
-processFormulas :: [String] -> IO ()
-processFormulas = mapM_ putStrLn
-
-processFormula :: String -> IO String
-processFormula = return
-
 
 listFormulas :: (ExitCode, ByteString, ByteString) -> IO [String]
 listFormulas input =
   return $
+  case input of
+    (ExitSuccess, out, _) -> mkStringList out
+    (ExitFailure code, _, err) -> errors code err
+
+processFormulas :: [String] -> IO ()
+processFormulas = mapM_ putStrLn
+
+processFormula :: String -> IO [String]
+processFormula formula = execBrewdeps >>= listDependents
+  where
+    execBrewdeps = readProcess $ proc "brew" ["uses", "--installed", formula]
+
+listDependents :: (ExitCode, ByteString, ByteString) -> IO [String]
+listDependents input =
+  return $
     case input of
-      (ExitSuccess, out, _) -> formulas out
+      (ExitSuccess, out, _) -> mkStringList out
       (ExitFailure code, _, err) -> errors code err
 
-formulas :: ByteString -> [String]
-formulas = splitOn "\n" . unpack
+processDependents :: [String] -> IO ()
+processDependents = mapM_ putStrLn
+
+processDependent :: String -> IO ()
+processDependent = putStrLn
+
+mkStringList :: ByteString -> [String]
+mkStringList = splitOn "\n" . unpack
 
 errors :: Int -> ByteString -> [String]
 errors code err = ["Error occured:", codeMessage code, errorMessage err]
