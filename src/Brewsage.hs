9a5f10a1128561421ff93e0b9ea1bfdb35ebf4e9
Gerd Reiss <gerdreiss@jscriptive.com>
2019-02-23 11:19:55 +0100

    formatting

--- src/Brewsage.hs
+++ src/Brewsage.hs
@@ -4,12 +4,11 @@ module Brewsage
   ( brewsage
   ) where
 
-import Data.ByteString.Lazy (ByteString)
-import Data.ByteString.Lazy.Char8 (unpack)
-import Data.List
-import Data.List.Split
-import System.Exit (ExitCode(ExitSuccess, ExitFailure))
-import System.Process.Typed
+import           Data.ByteString.Lazy       (ByteString)
+import           Data.ByteString.Lazy.Char8 (unpack)
+import           Data.List.Split            (splitOn)
+import           System.Exit                (ExitCode (ExitFailure, ExitSuccess))
+import           System.Process.Typed       (proc, readProcess)
 
 brewsage :: IO ()
 brewsage = readProcess "brew list" >>= listFormulas >>= processFormulas
@@ -17,9 +16,9 @@ brewsage = readProcess "brew list" >>= listFormulas >>= processFormulas
 listFormulas :: (ExitCode, ByteString, ByteString) -> IO [String]
 listFormulas input =
   return $
-    case input of
-      (ExitSuccess, out, _) -> toFormulaList out
-      (ExitFailure code, _, err) -> toErrorList code err
+  case input of
+    (ExitSuccess, out, _) -> toFormulaList out
+    (ExitFailure code, _, err) -> toErrorList code err
 
 processFormulas :: [String] -> IO ()
 processFormulas = mapM_ processFormula
@@ -45,7 +44,8 @@ toFormulaList :: ByteString -> [String]
 toFormulaList = splitOn "\n" . unpack
 
 toErrorList :: Int -> ByteString -> [String]
-toErrorList code err = ["Error occurred while invoking homebrew", codeMessage code, errorMessage err]
+toErrorList code err =
+  ["Error occurred while invoking homebrew", codeMessage code, errorMessage err]
 
 codeMessage :: Int -> String
 codeMessage code = "Code   -> " ++ show code
