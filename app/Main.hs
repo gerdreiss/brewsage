2acf48e682f36e4e71374ed402678671d1b801b0
Gerd Reiss <gerdreiss@jscriptive.com>
2019-08-03 10:00:18 +0200

    Formatting

--- app/Main.hs
+++ app/Main.hs
@@ -9,9 +9,13 @@ import           Data.Time
 main :: IO ()
 main = do
   start <- getCurrentTime
+
   readFormulasWithUsages >>= printUsed
+
   stop <- getCurrentTime
+
   print $ diffUTCTime stop start
+
   where
     printUsed = putStrLn . concatMap show . filter (null . dependents) . rights
              -- putStrLn . concatMap show . lefts
