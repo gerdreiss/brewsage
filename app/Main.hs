e9be25d59cf0df45697491825ecc5a4787171e60
Gerd Reiss <gerdreiss@jscriptive.com>
2019-08-03 18:25:05 +0200

    Finish deleting unused formulas

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,19 @@
 module Main where
 
-import           Control.Brew.Maintenance (askDeleteFormulas)
+import           Control.Brew.Maintenance (procFormulas)
 import           Control.Brew.Usage       (readFormulasWithUsages)
-
 import           Data.Brew
 import           Data.Either              (lefts, rights)
-import           Data.Time
+import           Data.Time                (diffUTCTime, getCurrentTime)
+import           System.IO                (hFlush, stdout)
 
 main :: IO ()
 main = do
+  putStr "Reading formula information... "
+  hFlush stdout
   start <- getCurrentTime
   formulas <- readFormulasWithUsages
   stop <- getCurrentTime
-  askDeleteFormulas . rights $ formulas
+  putStrLn $ "Done. Time: " ++ show (diffUTCTime stop start)
+  procFormulas . rights $ formulas
   putStrLn . concatMap show . lefts $ formulas
-  print $ diffUTCTime stop start
--- src/Control/Brew/Maintenance.hs
+++ src/Control/Brew/Maintenance.hs
@@ -1,18 +1,47 @@
-module Control.Brew.Maintenance where
+module Control.Brew.Maintenance
+  ( procFormulas
+  , procFormula
+  ) where
 
-import           Control.Monad
 import           Data.Brew
 import qualified Data.ByteString.Lazy.Char8 as C8
+import           Data.List                  (intercalate)
+import           System.Exit                (ExitCode (ExitFailure, ExitSuccess),
+                                             exitSuccess)
 import           System.IO                  (hFlush, stdout)
-
-askDeleteFormulas :: [BrewFormula] -> IO ()
-askDeleteFormulas formulas = putStrLn $ "processing formulas:\n" ++ concatMap show formulas
+import           System.Process.Typed       (proc, readProcess)
 
 --
 --
--- ask the user to delete the unused formula
+-- process the list of formulas
+procFormulas :: [BrewFormula] -> IO ()
+procFormulas formulas = do
+  putStrLn "Processing..."
+  mapM_ procFormula formulas
+
+--
+procFormula :: BrewFormula -> IO ()
+procFormula formula
+  | not . null . dependents $ formula = print formula
+  | otherwise = do
+    answer <- askDeleteFormula formula
+    case answer of
+      No   -> return ()
+      Yes  -> deleteFormula formula
+      Quit -> putStrLn "Exit..." >> exitSuccess
+      Que  -> putStrLn "Did not understand your answer. Try again." >> procFormula formula
+
+-- ask the user whether to delete the unused formula
 askDeleteFormula :: BrewFormula -> IO Answer
 askDeleteFormula formula = do
   putStr $ (C8.unpack . name $ formula) ++ " is not used by any other formula. Delete? (y/N) "
   hFlush stdout
   read <$> getLine
+
+-- delete the given formula
+deleteFormula :: BrewFormula -> IO ()
+deleteFormula formula = do
+  input <- readProcess $ proc "brew" ["uninstall", C8.unpack . name $ formula]
+  case input of
+    (ExitSuccess  , out,   _) -> print out
+    (ExitFailure _,   _, err) -> print err
