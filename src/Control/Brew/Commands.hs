f0b88579696ca697ab38bc6a4cd2451c90e68eec
reisg <gerd.reiss@leonteq.com>
2020-08-05 09:54:15 +0200

    dependencies

--- src/Control/Brew/Commands.hs
+++ src/Control/Brew/Commands.hs
@@ -3,6 +3,7 @@
 module Control.Brew.Commands
   ( listFormulas
   , listDependants
+  , listDependencies
   )
 where
 
@@ -13,17 +14,23 @@ import           System.Exit
 import           System.Process.Typed           ( proc
                                                 , readProcess
                                                 )
+import           Data.List.Safe
 
 type ReadProcessResult = (ExitCode, B.ByteString, B.ByteString)
+
 type ErrorOrFormulas = Either BrewError [BrewFormula]
 
 -- list installed homebrew formulas
 listFormulas :: IO ErrorOrFormulas
-listFormulas = procBrewResult <$> execBrewList
+listFormulas = processListResult <$> execBrewList
 
 -- list dependants of the given formula
 listDependants :: BrewFormula -> IO ErrorOrFormulas
-listDependants formula = procBrewResult <$> (execBrewUses . name $ formula)
+listDependants formula = processListResult <$> (execBrewUses . name $ formula)
+
+-- list dependencies of the given formula
+listDependencies :: BrewFormula -> IO ErrorOrFormulas
+listDependencies formula = processInfoResult <$> (execBrewInfo . name $ formula)
 
 -- execute "brew list"
 execBrewList :: IO ReadProcessResult
@@ -33,7 +40,20 @@ execBrewList = readProcess "brew list"
 execBrewUses :: B.ByteString -> IO ReadProcessResult
 execBrewUses formula = readProcess $ proc "brew" ["uses", "--installed", C8.unpack formula]
 
+-- execute "brew info" for the given formula
+execBrewInfo :: B.ByteString -> IO ReadProcessResult
+execBrewInfo formula = readProcess $ proc "brew" ["info", C8.unpack formula]
+
 -- process results of a brew command that returns a list of formulas
-procBrewResult :: ReadProcessResult -> ErrorOrFormulas
-procBrewResult (ExitSuccess     , out, _  ) = Right $ map (`BrewFormula` []) (C8.words out)
-procBrewResult (ExitFailure code, _  , err) = Left $ BrewError code err
+processListResult :: ReadProcessResult -> ErrorOrFormulas
+processListResult (ExitFailure code, _, err) = Left $ BrewError code err
+processListResult (ExitSuccess, out, _) = Right $ map (\s -> BrewFormula s [] []) (C8.words out)
+
+-- process results of a brew info command
+processInfoResult :: ReadProcessResult -> ErrorOrFormulas
+processInfoResult (ExitFailure code, _  , err) = Left $ BrewError code err
+processInfoResult (ExitSuccess     , out, _  ) = Right formulas where
+  formulas     = map (\dependency -> BrewFormula dependency [] []) dependencies
+  dependencies = case safeHead . safeTail . dropWhile (/= "==> Dependencies") . C8.lines $ out of
+    Nothing   -> []
+    Just line -> filter (C8.elem ',') $ C8.words line
--- src/Data/Brew.hs
+++ src/Data/Brew.hs
@@ -2,33 +2,37 @@ module Data.Brew where
 
 import qualified Data.ByteString.Lazy          as B
 import qualified Data.ByteString.Lazy.Char8    as C8
-
 import           Data.Char                      ( isLetter
                                                 , toLower
                                                 )
 import           Data.List                      ( intercalate )
 
-data BrewFormula =
-  BrewFormula
-    { name       :: B.ByteString
-    , dependants :: [BrewFormula]
-    }
+data BrewFormula = BrewFormula
+  { name :: B.ByteString,
+    dependencies :: [BrewFormula],
+    dependants :: [BrewFormula]
+  }
 
 instance Show BrewFormula where
   show formula = (C8.unpack . name $ formula) ++ case dependants formula of
     []       -> " is not used by any other formula."
     formulas -> " is used by " ++ formulaNames formulas ++ "."
-      where formulaNames formulas = intercalate ", " (map (C8.unpack . name) formulas)
+     where
+      formulaNames formulas =
+        intercalate ", " (map (C8.unpack . name) formulas)
 
-data BrewError =
-  BrewError
-    { code    :: Int
-    , message :: B.ByteString
-    }
+data BrewError = BrewError
+  { code :: Int,
+    message :: B.ByteString
+  }
 
 instance Show BrewError where
-  show error =
-    "Error occurred: code " ++ (show . code $ error) ++ " message " ++ (show . message $ error)
+  show error = concat
+    [ "Error occurred: code "
+    , show . code $ error
+    , " message "
+    , show . message $ error
+    ]
 
 data Answer
   = Yes
